625178507d87538973627544223d80c9d5a5d880
AlexisHenry <alexis.henry150357@gmail.com>
2023-02-07 21:25:44 +0100

    fix: constant export name

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,3 +1,3 @@
-export { token, testRespository, user, gh } from "./constants.js";
+export { token, testRepository, user, gh } from "./constants.js";
 export { getRepositories, getRepository, formatRepository } from "./repositories.js";
 export { getUser } from "./user.js";
